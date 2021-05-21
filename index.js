62f53ade945bed62c876bc36e83547ef2dab777e
Ethan <ethan.blumenthal@gmail.com>
2021-05-20 19:00:34 -0500

    Added CryptoCompare API for price data.

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const GeminiAPI = require('gemini-api').default;
+const CryptoCompareAPI = require('cryptocompare');
+global.fetch = require('node-fetch');
 require('dotenv').config();
+
+CryptoCompareAPI.setApiKey(process.env.CRYPTOCOMPARE_KEY);
 
 const geminiAPI = new GeminiAPI({
   key: process.env.GEMINI_KEY,
@@ -9,5 +13,9 @@ const geminiAPI = new GeminiAPI({
 
 geminiAPI
   .newOrder({ amount: 1, price: 10000, side: 'buy', symbol: 'btcusd' })
+  .then((res) => console.log(res))
+  .catch((err) => console.log(err));
+
+CryptoCompareAPI.coinList()
   .then((res) => console.log(res))
   .catch((err) => console.log(err));
